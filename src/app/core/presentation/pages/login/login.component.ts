e616b6284649bc705f7e11c777a5c270c1104131
Valdir Cezar <valdircezar312@gmail.com>
2021-05-10 22:01:49 -0300

    feat: add validation for login forms

--- src/app/core/presentation/pages/login/login.component.ts
+++ src/app/core/presentation/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email = ''
+  password = ''
+
+  emailValid = new FormControl('', [Validators.minLength(5), Validators.email])
+  passwordValid = new FormControl('', [Validators.minLength(5)])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  errorValidEmail() {
+    if(this.emailValid.invalid) {
+      return 'E-mail is required'
+    }
+    return false
+  }
+
+  errorValidPassword() {
+    if(this.passwordValid.invalid) {
+      return 'Password is required'
+    }
+    return false
   }
 
 }
